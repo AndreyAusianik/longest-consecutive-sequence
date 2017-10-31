72cfe573ca7d647d3edcf52d4be441047012e552
Andrei Ausianik <andrei.ausianik@iii.com>
2017-10-31 11:00:13 +0300

    LCS solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    const setLinks = new Map();
+    const sets = new Map();
+    array.forEach(l => {
+       if(setLinks.has(l)) {
+           return;
+       }
+
+       if(setLinks.has(l - 1) && setLinks.has(l + 1)) {
+           let leftSet = setLinks.get(l - 1),
+            rightSet = setLinks.get(l + 1);
+           while(leftSet !== setLinks.get(leftSet)) leftSet = setLinks.get(leftSet);
+           while(rightSet !== setLinks.get(rightSet)) rightSet = setLinks.get(rightSet);
+           setLinks.set(l, leftSet);
+           setLinks.set(rightSet, leftSet);
+           sets.set(leftSet, sets.get(leftSet) + sets.get(rightSet) + 1);
+           return;
+       }
+
+       if(setLinks.has(l - 1)) {
+           let set = setLinks.get(l - 1);
+           while(set !== setLinks.get(set)) set = setLinks.get(set);
+           setLinks.set(l, set);
+           sets.set(set, sets.get(set) + 1);
+
+           return;
+       }
+
+       if(setLinks.has(l + 1)) {
+           let set = setLinks.get(l + 1);
+           while(set !== setLinks.get(set)) set = setLinks.get(set);
+           setLinks.set(l, set);
+           sets.set(set, sets.get(set) + 1);
+           return;
+       }
+
+       setLinks.set(l, l);
+       sets.set(l, 1);
+    });
+    const longestSetLength = Math.max(...sets.values());
+    return longestSetLength > 1 ? longestSetLength : 0;
+};
